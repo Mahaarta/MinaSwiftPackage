d69fb65870060eed243dcae8d3ed99351d3569ef
Minata Mahaarta <minataputra@gmail.com>
2022-04-26 11:12:57 +0700

    add some mainContainer

---
+++ Sources/MinaPack/View.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by macbook on 26/04/22.
+//
+
+import Foundation
+import UIKit
+
+public class MinaPackView: UIView {
+    
+    @IBOutlet weak var mainContainer: UIView!
+    
+    public func haha() {
+        mainContainer.backgroundColor = .black
+    }
+}
