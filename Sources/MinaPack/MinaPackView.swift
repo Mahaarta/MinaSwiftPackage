4768c21dcbd9da3d2070e0f19cbba4dc56ca8d9d
Minata Mahaarta <minataputra@gmail.com>
2022-04-26 12:25:59 +0700

    add some propoerties to SPM view

--- Sources/MinaPack/MinaPackView.swift
+++ Sources/MinaPack/MinaPackView.swift
@@ -8,11 +8,35 @@
 import Foundation
 import UIKit
 
-public class MinaPackView: UIView {
+final class MinaPackView: UIView {
+    
+    private static let NIB_NAME = "MinaPackView"
     
     @IBOutlet weak var mainContainer: UIView!
     
-    public func haha() {
-        mainContainer.backgroundColor = .black
+    override func awakeFromNib() {
+        initWithNib()
+    }
+    
+    private func initWithNib() {
+        Bundle.main.loadNibNamed(MinaPackView.NIB_NAME, owner: self, options: nil)
+        mainContainer.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(mainContainer)
+        setupLayout()
+    }
+    
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+            mainContainer.topAnchor.constraint(equalTo: topAnchor),
+            mainContainer.leadingAnchor.constraint(equalTo: leadingAnchor),
+            mainContainer.bottomAnchor.constraint(equalTo: bottomAnchor),
+            mainContainer.trailingAnchor.constraint(equalTo: trailingAnchor),
+        ])
+    }
+    
+    var viewBackgroundColor: UIColor = UIColor.red {
+        didSet {
+            mainContainer.backgroundColor = viewBackgroundColor
+        }
     }
 }
